2fc66675f1ed6a751ca5ea72378b9a41290e699c
KHajili98 <khajili98@gmail.com>
2021-03-29 20:34:57 +0400

    tasks copied to this repo

--- Week3.Task/Week3.Task/Program.cs
+++ Week3.Task/Week3.Task/Program.cs
@@ -6,7 +6,124 @@ namespace Week3.Task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Task
+
+            #region 1 Bal Ortalamasinin Hesablanmasi
+            /*
+             * 1. Istifadeciden gelen 3 ferqli imtahan neticelerinin
+             * ortalamasinin 45-den  boyuk ve ya beraber olmasini
+             * yoxlayin.
+             *
+             * qeyd : if else istifade etmeyin burada
+             * 
+             * netice asagidaki kimi olmalidir.
+             *
+             *
+             * Netice : 30
+             * Netice : 50
+             * Netice : 40
+             *
+             *
+             * "Ortalama 45-e beraber ve ya boyukdurmu?" : "40"(Ortalamadir) : Beli ve ya Xeyr
+             */
+
+
+
+            /*
+            * 2.Bal Ortalamasinin Hesablanmasi
+            *
+            * istifadeciden ad ve soyadini ,
+            *  4 eded imtahan neticesini daxil etmesini isteyirik.
+            *
+            * netice asagidaki kimi olmalidir
+            *
+            * Ad ve Soyad : Parviz Aliyev
+            *
+            * Netice : 50
+            * Netice : 40
+            * Netice : 30
+            * Netice : 20
+            *
+            * Ortalama : 35
+            */
+
+            #endregion
+
+            #region 2. while ile consoledan daxil edilen ededden geriye dogru loop
+
+            #endregion
+
+            #region 3. while ile 0 ile 100 arasindaki tek ededlerin cemi
+
+
+            #endregion
+
+            #region 4. while ile faktorialin hesablanmasi
+
+            #endregion
+
+            #region 5. Mini login
+
+            /*
+             * Username ve password deye deyisenler teyin edin.
+             *
+             *  Username : admin
+             *  Password : 6gH123
+             *
+             * Istifadeciden  username ve Password daxil etmesini isteyin.
+             *
+             * Daxil edilen username ve passwordun teyin edilen
+             * deyisenlerin deyerine beraber olub olmamasini yoxlayin,
+             *
+             * Netice olaraq login emeliyyatinin ugurlu ve ya
+             *
+             *  daxil edilen username ve ya sifrenin dogru olmadigi mesajini verin.
+             *
+             *
+             */
+
+            #endregion
+
+            #region exam
+
+            /*
+             * 1. int num1=10;
+             *    int num2=20;
+             *    bool result = num1 > num2;
+             *
+             *  asagidakilardan hansi uygun gelir ?
+             *
+             *  num1 deyeri num2 deyerinden kicikdir ve bool tipinde olan result
+             *  deyiseni bu iki deyerin muqayisesinden false deyerini alir.
+             *
+             * num1 deyeri num2 deyerine beraberdir ve bool tipinde olan result
+             *  deyiseni true-dur.
+             *
+             ********************
+             * 2. int num1 = 100;
+             *
+             *    int num2 = 2;
+             *    
+             *    int result = (num1 + 10 ) / num2;
+             *    
+             *    Console.WriteLine(result);
+             *
+             * Result : 33;
+             * Result : 55;
+             * Result : 66;
+             * ******************
+             * 3. Boyuk beraberdir  ve beraber deyildir operatorlari hansilardir.
+             *
+             * =! , ==
+             * == , <=
+             * >= , <=
+             * >= , !=
+             */
+
+            #endregion
+
+            #endregion
+
         }
     }
 }
