5f9c68b5b3dcaabb9a1dbdd823a98c0454fa6824
KHajili98 <khajili98@gmail.com>
2021-03-29 20:13:34 +0400

    Creating Week3.Task project folder

---
+++ Week3.Task/Week3.Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week3.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
