cb7851fa6e066481104f38f113ec5649f4987edc
KHajili98 <khajili98@gmail.com>
2021-04-05 12:08:54 +0400

    1,2,3,5,6 solved

--- Week4.Task/Week4.Task/Program.cs
+++ Week4.Task/Week4.Task/Program.cs
@@ -15,8 +15,36 @@ namespace Week4.Task
              *
              * qeyd : manual edilecek/ hazir methodlarsiz
              */
-            //int[] intArray = { 2, 9, 4, 3, 5, 1, 7 };
 
+            // ------------------------------------------- Solution :
+
+            /*
+                        int[] intArray = { 2, 9, 4, 3, 5, 1, 7 };
+                        int temp = 0;
+
+
+
+                        for (int i = 0; i < intArray.Length; i++)
+                        {
+                            for (int j = i+1; j < intArray.Length; j++)
+                            {
+                                if (intArray[i] >intArray[j])
+                                {
+                                    temp = intArray[i];
+
+                                    intArray[i] = intArray[j];
+                                    intArray[j] = temp;
+                                }
+                            }
+                        }
+
+                        Console.WriteLine("Elementlerin artan sira ile manual wekilde sort edilmesi : \n");
+
+                        foreach (var item in intArray)
+                        {
+                            Console.WriteLine(item);
+                        }
+            */
             #endregion
 
             #region 2.
@@ -26,8 +54,34 @@ namespace Week4.Task
              *
              * qeyd : manual edilecek/ hazir methodlarsiz
              */
-            //int[] intArray = { 2, 9, 4, 3, 5, 1, 7 };
 
+            // ------------------------------------------- Solution :
+            /*
+                        int[] intArray = { 2, 9, 4, 3, 5, 1, 7 };
+                        int temp = 0;
+
+
+                        for (int i = 0; i < intArray.Length; i++)
+                        {
+                            for (int j = i + 1; j < intArray.Length; j++)
+                            {
+                                if (intArray[i] < intArray[j])
+                                {
+                                    temp = intArray[i];
+
+                                    intArray[i] = intArray[j];
+                                    intArray[j] = temp;
+                                }
+                            }
+                        }
+
+                        Console.WriteLine("Elementlerin azalan sira ile manual wekilde sort edilmesi : \n");
+
+                        foreach (var item in intArray)
+                        {
+                            Console.WriteLine(item);
+                        }
+            */
 
 
             #endregion
@@ -39,7 +93,40 @@ namespace Week4.Task
              *
              * qeyd : manual edilecek/ hazir methodlarsiz
              */
-            //int[] arr = { 5, 6, 9, 12, 15, 7, 3, 20, 14, 36, 24 };
+
+            //----------------------------------------------------- Solution :
+
+            /*
+                        int[] arr = { 5, 6, 9, 12, 15, 7, 3, 20, 14, 36, 24 };
+                        int temp = 0;
+                        var length = arr.Length;
+
+                        Console.WriteLine("Arrayin manual wekilde reverse olunmasi :");
+                        Console.WriteLine();
+                        Console.WriteLine("Ilkin array  :");
+
+                        foreach (var item in arr)
+                        {
+                            Console.Write(item + "\t");
+                        }
+
+
+
+                        for (int i = 0; i < length / 2 ; i++)
+                        {
+                            temp = arr[i];
+                            arr[i] = arr[length -i- 1];
+                            arr[length -i- 1] = temp;
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine("\nCevrilmiw array  : ");
+
+                        foreach (var item in arr)
+                        {
+                            Console.Write(item+"\t");
+                        }
+                        Console.WriteLine();
+            */
 
             #endregion
 
@@ -62,15 +149,49 @@ namespace Week4.Task
             /*
              * 1-10 qeder tek ededlerin kublarinin cemini yazin.
              */
-
+            //----------------------------------------------------- Solution :
+            /*
+                        int sum = 0;
+                        for (int i = 1; i <= 10; i++)
+                        {
+                            if (i % 2 == 1)
+                            {
+                                sum = sum + (i * i * i);
+                            }
+                        }
+                        Console.WriteLine("1-10 qeder tek ededlerin kublarinin cemini : "+sum);
+            */
 
             #endregion
 
             #region 6. yas hesablanmasi
+
             /*
              * dogum tarixini daxil eden istifedecinin
              * yasini hesablayib ekrana cap edin
              */
+
+            //----------------------------------------------------- Solution :
+
+/*
+            DateTime today = DateTime.Today;
+
+            Console.WriteLine("dogum tarixini daxil edin (gun,ay,il) : ");
+
+            string birthDay=  Console.ReadLine();
+            
+            string[] birthDayArray = new string[3];
+
+            birthDay.Split(',').CopyTo(birthDayArray, 0);
+
+            var age = today.Year - Convert.ToInt32(birthDayArray[2]);
+
+            Console.WriteLine("Sizin yawiniz : "+age);
+*/
+            
+
+
+
 
 
             #endregion
@@ -80,6 +201,8 @@ namespace Week4.Task
              * istifadeci terefinden daxil edilen iki musbet ededin hasilini vurma emeliyyati ile
              * deyil toplama emeliyyati ile ekrana cap edin.
              */
+
+
 
 
             #endregion
