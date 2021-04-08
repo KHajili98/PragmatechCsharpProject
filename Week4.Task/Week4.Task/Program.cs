30cf1a457be0ff1dcbeb4f796138c2e9f70112bd
KHajili98 <khajili98@gmail.com>
2021-04-08 14:45:51 +0400

    last changes completed

--- Week4.Task/Week4.Task/Program.cs
+++ Week4.Task/Week4.Task/Program.cs
@@ -145,129 +145,129 @@ namespace Week4.Task
 
             //----------------------------------------------------- Solution :
 
-            /*
-            while (true)
-            {
-                var bankNotes = new int[] { 200, 100, 50, 20, 10, 5, 1 };
-                Console.Write("bankomatdan cixarmaq istediyi meblegi daxil edin : ");
-                int inputMoney = Convert.ToInt32(Console.ReadLine());
-                if (string.IsNullOrEmpty(Convert.ToString(inputMoney)) || string.IsNullOrWhiteSpace(Convert.ToString(inputMoney)))  break;
+            /*  
+           while (true)
+           {
+               var bankNotes = new int[] { 200, 100, 50, 20, 10, 5, 1 };
+               Console.Write("bankomatdan cixarmaq istediyi meblegi daxil edin : ");
+               int inputMoney = Convert.ToInt32(Console.ReadLine());
+               if (string.IsNullOrEmpty(Convert.ToString(inputMoney)) || string.IsNullOrWhiteSpace(Convert.ToString(inputMoney))) break;
 
-                for (int i = 0; i < bankNotes.Length; i++)
-                {
-                    if (inputMoney >= bankNotes[i])
-                    {
-                        int bankNotesCount = inputMoney / bankNotes[i];
-                        inputMoney -= bankNotesCount * bankNotes[i];
-                        Console.WriteLine(bankNotesCount + " - " + bankNotes[i]);
-                    }
-                }
-            }
-            */
+               for (int i = 0; i < bankNotes.Length; i++)
+               {
+                   if (inputMoney >= bankNotes[i])
+                   {
+                       int bankNotesCount = inputMoney / bankNotes[i];
+                       inputMoney -= bankNotesCount * bankNotes[i];
+                       Console.WriteLine(bankNotesCount + " eded - " + bankNotes[i] + " AZN");
+                   }
+               }
+           }
+              */
 
-             #endregion
+            #endregion
 
             #region 5. tek ededlerin kublari cemi
             /*
              * 1-10 qeder tek ededlerin kublarinin cemini yazin.
             */
-             //----------------------------------------------------- Solution :
-                /*
-                int sum = 0;
-                for (int i = 1; i <= 10; i++)
+            //----------------------------------------------------- Solution :
+            /*
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 1)
                 {
-                    if (i % 2 == 1)
-                    {
-                        sum = sum + (i * i * i);
-                    }
+                    sum = sum + (i * i * i);
                 }
-                Console.WriteLine("1-10 qeder tek ededlerin kublarinin cemini : "+sum);
-                 */
+            }
+            Console.WriteLine("1-10 qeder tek ededlerin kublarinin cemini : "+sum);
+             */
 
-    #endregion
+            #endregion
 
             #region 6. yas hesablanmasi
 
-                /*
-                 * dogum tarixini daxil eden istifedecinin
-                 * yasini hesablayib ekrana cap edin
-                 */
+            /*
+             * dogum tarixini daxil eden istifedecinin
+             * yasini hesablayib ekrana cap edin
+             */
 
-                 //----------------------------------------------------- Solution :
+            //----------------------------------------------------- Solution :
 
-             /*
-                DateTime today = DateTime.Today;
+            /*
+            DateTime today = DateTime.Today;
 
-                Console.WriteLine("dogum tarixini daxil edin (gun,ay,il) : ");
+            Console.WriteLine("dogum tarixini daxil edin (gun,ay,il) : ");
 
-                string birthDay=  Console.ReadLine();
+            string birthDay = Console.ReadLine();
 
-                string[] birthDayArray = new string[3];
+            string[] birthDayArray = new string[3];
 
-                birthDay.Split(',').CopyTo(birthDayArray, 0);
+            birthDay.Split(',').CopyTo(birthDayArray, 0);
 
-                var age = today.Year - Convert.ToInt32(birthDayArray[2]);
+            var age = today.Year - Convert.ToInt32(birthDayArray[2]);
 
-                Console.WriteLine("Sizin yawiniz : "+age);
-               */
-
-
+            Console.WriteLine("Sizin yawiniz : " + age);
+            */
 
 
 
 
-    #endregion
+
+
+            #endregion
 
             #region 7.
-                 /*
-                 * istifadeci terefinden daxil edilen iki musbet ededin hasilini vurma emeliyyati ile
-                  * deyil toplama emeliyyati ile ekrana cap edin.
-                 */
+            /*
+            * istifadeci terefinden daxil edilen iki musbet ededin hasilini vurma emeliyyati ile
+             * deyil toplama emeliyyati ile ekrana cap edin.
+            */
 
-                 //----------------------------------------------------- Solution :
+            //----------------------------------------------------- Solution :
 
-                 /*
-                int sum = 0;
-                int firstNumber = 0;
-                int  secondNumber = 0;
-
-
-
-                while (true)
-                {
-                    Console.WriteLine("Birinci vurugu elave edin : ");
-                     firstNumber = Convert.ToInt32(Console.ReadLine());
-                    if (firstNumber < 0)
-                        Console.Write("Birinci vuruq musbet eded deyil. Zehmet olmasa musbet eded daxil edin. ");
-                    else
-                        break;
-                }
-
-                while (true)
-                {
-
-
-                    Console.WriteLine("Ikinci vurugu elave edin : ");
-                     secondNumber = Convert.ToInt32(Console.ReadLine());
-                    if (secondNumber < 0)
-                        Console.Write("Ikinci vuruq musbet eded deyil. Zehmet olmasa musbet eded daxil edin. ");
-                    else
-                        break;
-                }
-
-                for (var i = secondNumber; i > 0; i--)
-                {
-                    sum += firstNumber;
-                }
-
-                Console.WriteLine("Daxil edilen vuruqlarin hasili : " +sum);
-                */
+            /*
+           int sum = 0;
+           int firstNumber = 0;
+           int  secondNumber = 0;
 
 
 
-             #endregion
-             #endregion
-}
+           while (true)
+           {
+               Console.WriteLine("Birinci vurugu elave edin : ");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+               if (firstNumber < 0)
+                   Console.Write("Birinci vuruq musbet eded deyil. Zehmet olmasa musbet eded daxil edin. ");
+               else
+                   break;
+           }
+
+           while (true)
+           {
+
+
+               Console.WriteLine("Ikinci vurugu elave edin : ");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+               if (secondNumber < 0)
+                   Console.Write("Ikinci vuruq musbet eded deyil. Zehmet olmasa musbet eded daxil edin. ");
+               else
+                   break;
+           }
+
+           for (var i = secondNumber; i > 0; i--)
+           {
+               sum += firstNumber;
+           }
+
+           Console.WriteLine("Daxil edilen vuruqlarin hasili : " +sum);
+           */
+
+
+
+            #endregion
+            #endregion
+        }
     }
     }
 
