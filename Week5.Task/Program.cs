370689cb8ab437a922f07cdd94b11abb237886cb
KHajili98 <khajili98@gmail.com>
2021-04-12 21:04:53 +0400

    task 2 added without solution

--- Week5.Task/Program.cs
+++ Week5.Task/Program.cs
@@ -2,10 +2,18 @@
 
 namespace Week5.Task
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            #region Week 5 Tasks
+
+
+
+            #region 1. Havada bulut dimplom isini unut
             /*
              * Telebinin adini soyadini 3 imtahan neticesini
              * (((her hansisa imtahan neticesi daxil edilmezse default deyer 51 verilecek,
@@ -17,6 +25,48 @@ namespace Week5.Task
              * Taski mumkun derece kicik methodlara bolerek her methoda verilen adi ehtiva eden isler gorulsun.
              * Student classi yaradaraq method-lari orada yazib Program classi icerisinde Main methodunda istifade edeceksiz.
              */
+
+            #endregion
+
+
+            #region 2. para pul el cerki 2 yakinda sinemalarda
+            /*
+             * Evvelceden teyin edilen pin vasitesile ATM-ye yaxinlasan vetendas pini daxil edir.
+             * Qarsinina cixan menu-dan balansina baxa, negd pul cixarda, ve ya umumi hesabdan cixaris
+             * isteye biler.Secilen her hansi bir emeliyyatdan sonra istifadeci yeniden menuya- geri done bilmelidir.
+             *
+             *
+             * login
+             *
+             * pin:
+             * pin sadece reqemlerden ibaret olmalidir // 1234
+             * pin sadece 4 reqemden ibaret olmalidir.
+             *
+             *
+             * Menu
+             *
+             * 1.Balans:
+             * (evvelceden standart olaraq balans 1000 azn olaraq nezerde tutulur.)
+             *
+             * 2.Negd pulun verilmesi:
+             *  Istifadeci ATM-den min 1 manat max 1000 manat ceke biler.Balans yoxlamasi olacaq,
+             *  daxil edilen mebleg hansi esginasdan nece eded olacaq o sekilde netice olaraq gosterilir.
+             *   
+             *
+             * 3.Balansdan cixaris:
+             *   Eger balansdan mexaric olubsa mexaric olunan meblegi ve hemin tarixi cap edin,
+             *   Yox eger hele balansdan mexaric olunmayibsa bu haqqda mesaj gosterin.
+             *
+             *
+             * Qeyd: consoledan daxil edilenler yalniz eded ola biler bular yoxlayanacaq ,
+             * mumkun derece methodlarla isleyin
+             *   
+             */
+            #endregion
+
+
+            #endregion
+
         }
     }
 }
