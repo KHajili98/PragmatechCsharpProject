252e52f1b17abb2f287373e880e6a5ee2573ecc6
KHajili98 <khajili98@gmail.com>
2021-05-27 20:01:45 +0400

    input form design is done

--- Week8.Tasks/AddNewCustomer/Form1.Designer.cs
+++ Week8.Tasks/AddNewCustomer/Form1.Designer.cs
@@ -29,13 +29,127 @@ namespace AddNewCustomer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.surnameLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.surnameInput = new System.Windows.Forms.TextBox();
+            this.phoneInput = new System.Windows.Forms.TextBox();
+            this.emailInput = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(54, 36);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(49, 20);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name";
+            this.nameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // surnameLabel
+            // 
+            this.surnameLabel.AutoSize = true;
+            this.surnameLabel.Location = new System.Drawing.Point(54, 136);
+            this.surnameLabel.Name = "surnameLabel";
+            this.surnameLabel.Size = new System.Drawing.Size(67, 20);
+            this.surnameLabel.TabIndex = 1;
+            this.surnameLabel.Text = "Surname";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(413, 36);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(50, 20);
+            this.phoneLabel.TabIndex = 2;
+            this.phoneLabel.Text = "Phone";
+            this.phoneLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(413, 130);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(46, 20);
+            this.emailLabel.TabIndex = 3;
+            this.emailLabel.Text = "Email";
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(54, 71);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(125, 27);
+            this.nameInput.TabIndex = 4;
+            // 
+            // surnameInput
+            // 
+            this.surnameInput.Location = new System.Drawing.Point(54, 177);
+            this.surnameInput.Name = "surnameInput";
+            this.surnameInput.Size = new System.Drawing.Size(125, 27);
+            this.surnameInput.TabIndex = 5;
+            // 
+            // phoneInput
+            // 
+            this.phoneInput.Location = new System.Drawing.Point(413, 71);
+            this.phoneInput.Name = "phoneInput";
+            this.phoneInput.Size = new System.Drawing.Size(125, 27);
+            this.phoneInput.TabIndex = 6;
+            // 
+            // emailInput
+            // 
+            this.emailInput.Location = new System.Drawing.Point(413, 177);
+            this.emailInput.Name = "emailInput";
+            this.emailInput.Size = new System.Drawing.Size(125, 27);
+            this.emailInput.TabIndex = 7;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(219, 251);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(170, 58);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "SAVE";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(622, 396);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.emailInput);
+            this.Controls.Add(this.phoneInput);
+            this.Controls.Add(this.surnameInput);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.surnameLabel);
+            this.Controls.Add(this.nameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label surnameLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.TextBox surnameInput;
+        private System.Windows.Forms.TextBox phoneInput;
+        private System.Windows.Forms.TextBox emailInput;
+        private System.Windows.Forms.Button saveButton;
     }
 }
 
--- Week8.Tasks/AddNewCustomer/Form1.cs
+++ Week8.Tasks/AddNewCustomer/Form1.cs
@@ -17,5 +17,24 @@ namespace AddNewCustomer
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
