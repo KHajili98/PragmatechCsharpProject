c7577eb8aea3eb561af8dd0bcd1fc2a72c774bb1
KHajili98 <khajili98@gmail.com>
2021-06-07 01:54:28 +0400

    week10 task - first part is ready

---
+++ Week10.Tasks/WorkingWithSystemIO/MainPage.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace WorkingWithSystemIO
+{
+    partial class MainPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.folderBrowserDialogOpenFile = new System.Windows.Forms.FolderBrowserDialog();
+            this.dataGridViewFileTable = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFileTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(428, 334);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(183, 40);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // dataGridViewFileTable
+            // 
+            this.dataGridViewFileTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFileTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Type});
+            this.dataGridViewFileTable.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridViewFileTable.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewFileTable.Name = "dataGridViewFileTable";
+            this.dataGridViewFileTable.RowHeadersWidth = 51;
+            this.dataGridViewFileTable.RowTemplate.Height = 29;
+            this.dataGridViewFileTable.Size = new System.Drawing.Size(1100, 218);
+            this.dataGridViewFileTable.TabIndex = 1;
+            // 
+            // Name
+            // 
+            this.Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Name.HeaderText = "Name";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            // 
+            // Type
+            // 
+            this.Type.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Type.HeaderText = "Type";
+            this.Type.MinimumWidth = 6;
+            this.Type.Name = "Type";
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 656);
+            this.Controls.Add(this.dataGridViewFileTable);
+            this.Controls.Add(this.btnOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Page";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFileTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogOpenFile;
+        private System.Windows.Forms.DataGridView dataGridViewFileTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+    }
+}
+
---
+++ Week10.Tasks/WorkingWithSystemIO/MainPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkingWithSystemIO
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+          
+            if (folderBrowserDialogOpenFile.ShowDialog() == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialogOpenFile.SelectedPath))
+            {
+                string[] files = Directory.GetFiles(folderBrowserDialogOpenFile.SelectedPath);
+                string[] folders = Directory.GetDirectories(folderBrowserDialogOpenFile.SelectedPath);
+
+
+                foreach (var item in folders)
+                {
+                    var name = Path.GetFileName(item);
+                    dataGridViewFileTable.Rows.Add(name,"Folder");
+                }
+                foreach (var item in files)
+                {
+                    var name = Path.GetFileName(item);
+                    var extension = Path.GetExtension(item);
+                    // dataGridViewFileTable.Rows.Add(name.Split('.')[0], name.Split('.')[1]);//her columna uygun add elemek 
+                    dataGridViewFileTable.Rows.Add(name,"FILE is :    "+ extension);
+                }
+               
+           
+               
+            }
+        }
+    }
+}
