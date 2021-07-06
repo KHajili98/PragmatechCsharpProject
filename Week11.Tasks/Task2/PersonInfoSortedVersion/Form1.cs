4609c3f60a7a86c935c806f08b4f0a28c3ccae86
KHajili98 <khajili98@gmail.com>
2021-07-06 15:52:00 +0400

    week 11 task 2 - person model added

---
+++ Week11.Tasks/Task2/PersonInfoSortedVersion/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+/*
+````````2. Person classi yaratmalisiz 
+````````id, 
+````````name, 
+````````surname, 
+````````birthdate, 
+````````country , 
+````````city, 
+````````phone, 
+````````email,
+````````profession, Dizayn elediyiniz formApp de FakeData istifade ederek en az 1000 eded data yaradilib dataGride yerlesdirilecek (orderBY(profession)),
+````````dataGrid-in ust hissesinde olan searchTXTBox dan daxil edilen deyere gore ilk 3 herf daxil edildikden sonra cedvelde deyere sahib
+````````data gosterilmeli yoxdursa bu haqqda melumat verilmelidir , hemcinin cedvelin ustunde olan listBoxdan name, profession, Country deyerlerinden biri secilerse ona gore siralama olacaq
+
+
+ * */
+
+
+namespace PersonInfoSortedVersion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
---
+++ Week11.Tasks/Task2/PersonInfoSortedVersion/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfoSortedVersion
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Profession { get; set; }
+    }
+}
